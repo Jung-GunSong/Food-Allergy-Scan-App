de0d5b14927cf02ed7c858e34a65a7d31a57604c
Jung-GunSong <jsong781@gmail.com>
2024-02-10 22:47:41 -0500

    added camera screen file

---
+++ lib/camera_screen.dart
@@ -0,0 +1,40 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+class CameraScreen extends StatefulWidget {
+  const CameraScreen({super.key});
+
+  @override
+  CameraScreenState createState() => CameraScreenState();
+}
+
+class CameraScreenState extends State<CameraScreen> {
+  late CameraController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeCamera();
+  }
+
+  void _initializeCamera() async {
+    CameraDescription description = await availableCameras().then((cameras) => cameras[0]);
+    _controller = CameraController(description, ResolutionPreset.medium);
+    await _controller.initialize();
+    if (!mounted) {
+      return;
+    }
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_controller == null) {
+      return Container();
+    }
+    return AspectRatio(
+      aspectRatio: _controller.value.aspectRatio,
+      child: CameraPreview(_controller),
+    );
+  }
+}
